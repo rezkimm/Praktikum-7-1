02ef28e9f321adbd43704bb2b404bdcb808a2f54
rezkimm <rizki.snutz@gmail.com>
2016-11-14 10:28:34 +0700

    Add files via upload

---
+++ praktikum_7-1.ino
@@ -0,0 +1,15 @@
+int val=0;
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(9,1);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+val= analogRead(A0);
+analogWrite(9, val/4);
+Serial.print("PWM Value = ");
+Serial.println(val,DEC);
+delay(500);
+}
